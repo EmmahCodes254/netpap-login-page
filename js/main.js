7057652df20bbe2511b494fd6ab0c5fe48aa9425
shockwave <mwitidenis@rafikihost.co.ke>
2020-10-03 18:13:29 +0300

    initial commit  for landing page

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.testimonial_carousel_section').owlCarousel({
+    loop:true,
+    margin:20,
+    autoplay:true, 
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
